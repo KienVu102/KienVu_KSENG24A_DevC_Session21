c39ca49f9dec49a42189de2aa0fb8006a8855e03
KienVu102 <vudinhkien1002@gmail.com>
2024-12-25 11:33:40 +0700

    Kiên Vũ upload Session21_Ex03

---
+++ Session21_Ex03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	FILE  *fptr;
+	fptr = fopen("bt01.txt", "a");
+    char string[100]; 
+    fgets(string, sizeof string, stdin);
+    fprintf(fptr, "%s", string);
+    
+    fclose(fptr);
+	
+	return 0;
+}
+
